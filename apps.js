9e0dee94b93c6660d4912217675240543d9f27bd
tonmoy <tonmoy3673@gmail.com>
2024-05-21 13:34:31 +0600

    :sparkles: implemented toggle

--- apps.js
+++ apps.js
@@ -1,1 +1,11 @@
-console.log('connected')
+
+// ============== Toggle Button Function==============//
+document.getElementById('user-menu-button').addEventListener('click',function(){
+    const userMenuList=document.getElementById('user-menu-item');
+    const isOpen = userMenuList.classList.contains('hidden');
+    if (isOpen) {
+        userMenuList.classList.remove('hidden');
+    } else {
+        userMenuList.classList.add('hidden');
+    }
+});
